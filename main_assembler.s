f3cd732d21e1d2ee6f14078e5a76bd3fa8940f8d
ReichThomas <47111814+ReichThomas@users.noreply.github.com>
2021-10-04 16:57:54 +0200

    newest change workspace

--- main_assembler.s
+++ main_assembler.s
@@ -21,7 +21,24 @@
 		PUBLIC  main
 				
 
-main
+main 
 
+move_eg   MOV     r0, #10            ; r0 = d'10'
+          MOV     r1, r0              ; r1 = r0
+          MOV     r0, r1, LSL #4      ; r0 = r1 * 16
+          MOV     r0, r1, ASR #1      ; r0 = signed(r1 / 2)
+          MOV     r2, #8              ; r2 = 8
+          MOV     r0, r1, LSL r2      ; r0 = r1 * 2^r2
+          MOV     r0, r1, ASR r2      ; r0 = signed(r1 / 2^r2)
+
+          MOV     r0, #0
+          MOV     r1, #0
+          MOV     r2, r1
+          MOVS    r3, #0              ; r3 = 0, update APSR
+
+          ADD     r0, r0, #256        ; r0 = r0 + 256
+          ADD     r1, r1, r0, LSL #2  ; r1 = r1 + (4 * r0)
+          MOV     r2, r0, LSR #6      ; r2 = r0 / 64
+          SUBS    r3, r1, r0          ; r2 = r1 - r0, update APSR
 
 	END
