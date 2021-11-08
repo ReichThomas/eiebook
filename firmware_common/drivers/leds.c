c682fa8e7924dec5699f68c1f5b14c3a9f962cec
ReichThomas <47111814+ReichThomas@users.noreply.github.com>
2021-11-08 16:17:14 +0100

    ChangeO_08112021

--- firmware_common/drivers/leds.c
+++ firmware_common/drivers/leds.c
@@ -123,6 +123,125 @@ void LedOn(LedNameType eLED_)
   Led_asControl[(u8)eLED_].eMode = LED_NORMAL_MODE;
 } /* end LedOn() */
   
+/*!----------------------------------------------------------------------------------------------------------------------
+@fn void LedOff(LedNameType eLED_)
+
+@brief Turn the specified LED off.
+
+This function automatically takes care of the active low vs. active high LEDs.
+It works immediately (it does not require the main application
+loop to be running). 
+
+Currently it only supports one LED at a time.
+
+Example:
+
+LedOff(BLUE);
+
+
+Requires:
+- Definitions in G_asBspLedConfigurations[eLED_] and Led_asControl[eLED_] are correct
+
+@param eLED_ is a valid LED index
+
+Promises:
+- eLED_ is turned off 
+- eLED_ is set to LED_NORMAL_MODE mode
+
+*/
+void LedOff(LedNameType eLED_)
+{
+  u32 *pu32OffAddress;
+
+  /* Configure set and clear addresses */
+  if(G_asBspLedConfigurations[eLED_].eActiveState == LED_ACTIVE_HIGH)
+  {
+    /* Active high LEDs use CODR to turn off */
+    pu32OffAddress = (u32*)(&(AT91C_BASE_PIOA->PIO_CODR) + G_asBspLedConfigurations[(u8)eLED_].ePort);
+  }
+  else
+  {
+    /* Active low LEDs use SODR to turn off */
+    pu32OffAddress = (u32*)(&(AT91C_BASE_PIOA->PIO_SODR) + G_asBspLedConfigurations[(u8)eLED_].ePort);
+  }
+  
+  /* Clear the bit corresponding to eLED_ */
+	*pu32OffAddress = G_asBspLedConfigurations[(u8)eLED_].u32BitPosition;
+
+  /* Always set the LED back to LED_NORMAL_MODE mode */
+	Led_asControl[(u8)eLED_].eMode = LED_NORMAL_MODE;
+  
+} /* end LedOff() */
+
+
+/*!----------------------------------------------------------------------------------------------------------------------
+@fn void LedToggle(LedNameType eLED_)
+
+@brief Toggles the specified LED from on to off or vise-versa.
+
+This function automatically takes care of the active low vs. active high LEDs.
+It works immediately (it does not require the main application
+loop to be running). 
+
+Currently it only supports one LED at a time.
+
+Example:
+
+LedToggle(BLUE);
+
+
+Requires:
+- Write access to PIOx_ODSR is enabled
+
+@param eLED_ is a valid LED index
+
+Promises:
+- eLED_ is toggled 
+- eLED_ is set to LED_NORMAL_MODE
+
+*/
+void LedToggle(LedNameType eLED_)
+{
+  u32 *pu32Address = (u32*)(&(AT91C_BASE_PIOA->PIO_ODSR) + G_asBspLedConfigurations[eLED_].ePort);
+
+  *pu32Address ^= G_asBspLedConfigurations[(u8)eLED_].u32BitPosition;
+  
+  /* Set the LED to LED_NORMAL_MODE mode */
+	Led_asControl[(u8)eLED_].eMode = LED_NORMAL_MODE;
+
+} /* end LedToggle() */
+
+
+/*!----------------------------------------------------------------------------------------------------------------------
+@fn void LedBlink(LedNameType eLED_, LedRateType eBlinkRate_)
+
+@brief Sets eLED_ to BLINK mode with the rate given.
+
+BLINK mode requires the main loop to be running at 1ms period. If the main
+loop timing is regularly off, the blinking timing may be affected although
+unlikely to a noticeable degree.  
+
+Example to blink the PURPLE LED at 1Hz:
+
+LedBlink(PURPLE, LED_1HZ);
+
+
+Requires:
+@param eLED_ is a valid LED index
+@param eBlinkRate_ is an allowed blinking rate from LedRateType
+
+Promises:
+- eLED_ is set to LED_BLINK_MODE at the blink rate specified
+
+*/
+void LedBlink(LedNameType eLED_, LedRateType eBlinkRate_)
+{
+	Led_asControl[(u8)eLED_].eMode = LED_BLINK_MODE;
+	Led_asControl[(u8)eLED_].eRate = eBlinkRate_;
+	Led_asControl[(u8)eLED_].u16Count = eBlinkRate_;
+
+} /* end LedBlink() */
+
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 /*! @protectedsection */                                                                                            
